53b9aa3cdc02287d39e89a8788691a7bd629899b
luongvan76 <luongcongvan2@gmail.com>
2023-08-12 16:21:11 +0700

    Van, Nhi

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,12 +1,18 @@
-var app = angular.module('MyApp', ['ngRoute']);
+var app = angular.module("MyApp", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: './Views/home.html',
-            controller: 'HomeController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+  $routeProvider
+    .when("/", {
+      templateUrl: "./views/home.html",
+      controller: "HomeController",
+    })
+    .when("/seeker", {
+      templateUrl: "./views/seeker/seeker.html",
+    })
+    .when("/job_details", {
+      templateUrl: "./views/seeker/job_details.html",
+    })
+    .otherwise({
+      redirectTo: "/",
+    });
 });
--- assets/js/directive.js
+++ assets/js/directive.js
@@ -1,21 +1,23 @@
-
-app.directive('headingPage', () => {
-    return {
-        restrict: 'E',
-        templateUrl: './views/header.html',
-    }
+app.directive("headingPage", () => {
+  return {
+    restrict: "E",
+    templateUrl: "./views/header.html",
+  };
 });
 
-app.directive('footerPage', () => {
-    return {
-        restrict: 'E',
-        templateUrl: './views/footer.html',
-    }
+app.directive("footerPage", () => {
+  return {
+    restrict: "E",
+    templateUrl: "./views/footer.html",
+  };
 });
 
-app.directive('databg', () =>{
-    /* 9. data-background */
-    $("[data-background]").each(function () {
-        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-        });  
+app.directive("databg", () => {
+  /* 9. data-background */
+  $("[data-background]").each(function () {
+    $(this).css(
+      "background-image",
+      "url(" + $(this).attr("data-background") + ")"
+    );
+  });
 });
