0640b8be5fc54dbce9b4e9947df2c35af2be908b
yu12101007 <yu12101007@yersin.edu.vn>
2023-08-29 21:36:40 +0700

    login_employers

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,6 +10,38 @@ app.config(function ($routeProvider) {
             templateUrl: './Views/job_listing.html',
             controller: 'HomeController'
         })
+        .when('/signup', {
+            templateUrl: './Views/signup.html',
+            controller: 'HomeController'
+        })
+        .when('/signupemployers', {
+            templateUrl: './Views/signup_employers.html',
+            controller: 'HomeController'
+        })
+        .when('/account', {
+            templateUrl: './Views/account.html',
+            controller: 'HomeController'
+        })
+        .when('/login', {
+            templateUrl: './Views/login.html',
+            controller: 'HomeController'
+        })
+        .when('/post', {
+            templateUrl: './Views/post.html',
+            controller: 'HomeController'
+        })
+        .when('/postmanagement', {
+            templateUrl: './Views/post_management.html',
+            controller: 'HomeController'
+        })
+        .when('/profile', {
+            templateUrl: './Views/profile.html',
+            controller: 'HomeController'
+        })
+        .when('/profilepassword', {
+            templateUrl: './Views/profile_password.html',
+            controller: 'HomeController'
+        })
         .otherwise({
             redirectTo: '/'
         });
