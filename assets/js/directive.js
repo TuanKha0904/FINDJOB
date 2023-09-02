ae0a757f94a01d5101e8acefb0a1c7385f4759c7
TuanKha0904 <kha090403@gmail.com>
2023-09-02 11:35:28 +0700

    Xây dựng trang find a jobs, chỉnh sửa homepage

--- assets/js/directive.js
+++ assets/js/directive.js
@@ -28,10 +28,104 @@ app.directive("leftContentPageAdmin", () => {
 
 app.directive("databg", () => {
   /* 9. data-background */
-  $("[data-background]").each(function () {
-    $(this).css(
-      "background-image",
-      "url(" + $(this).attr("data-background") + ")"
-    );
-  });
+  return {
+    restrict: "A",
+    link: function(scope, element, attrs) {
+      // Lấy giá trị thuộc tính 'data-background' của phần tử
+      var dataBgValue = element.attr("data-background");
+      if (dataBgValue) {
+        // Đặt thuộc tính 'background-image' của phần tử
+        element.css("background-image", "url(" + dataBgValue + ")");
+      }
+    }
+  };
+});
+
+app.directive("range", () => {
+  return {
+    restrict: "A",
+    link: function (scope, element, attrs) {
+      var $range = $(".js-range-slider"),
+        $inputFrom = $(".js-input-from"),
+        $inputTo = $(".js-input-to"),
+        instance,
+        min = 0,
+        max = 5000,
+        from = 10,
+        to = 1000;
+
+      $range.ionRangeSlider({
+        type: "double",
+        min: min,
+        max: max,
+        from: 0,
+        to: 5000,
+        prefix: '',
+        onStart: updateInputs,
+        onChange: updateInputs,
+        step: 1,
+        prettify_enabled: true,
+        prettify_separator: ".",
+        values_separator: " - ",
+        force_edges: true
+
+
+      });
+
+      instance = $range.data("ionRangeSlider");
+
+      function updateInputs(data) {
+        from = data.from;
+        to = data.to;
+
+        $inputFrom.prop("value", from);
+        $inputTo.prop("value", to);
+      }
+
+      $inputFrom.on("input", function () {
+        var val = $(this).prop("value");
+
+        // validate
+        if (val < min) {
+          val = min;
+        } else if (val > to) {
+          val = to;
+        }
+
+        instance.update({
+          from: val
+        });
+      });
+
+      $inputTo.on("input", function () {
+        var val = $(this).prop("value");
+
+        // validate
+        if (val < from) {
+          val = from;
+        } else if (val > max) {
+          val = max;
+        }
+
+        instance.update({
+          to: val
+        });
+      });
+    }
+  }
+});
+
+app.directive("nselect", () => {
+  /* 6. Nice Selectorp  */
+  return {
+    restrict: "A",
+    link: function (scope, element, attrs) {
+      // Thực hiện mã của bạn ở đây để áp dụng Nice Selector cho phần tử select
+      var nice_Select = $('select');
+      if (nice_Select.length) {
+        nice_Select.niceSelect();
+      }
+      element.niceSelect();
+    }
+  };
 });
