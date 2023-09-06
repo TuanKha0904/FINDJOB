a5fa5c85da7df11d1fcc81e92b82cf406d942252
TuanKha0904 <kha090403@gmail.com>
2023-09-07 01:06:00 +0700

    update roote page

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,52 +4,52 @@ app.config(function ($routeProvider) {
 
     $routeProvider
         .when('/', {
-            templateUrl: './Views/home.html',
+            templateUrl: 'Views/home.html',
             controller: 'HomeController'
         })
         .when('/joblisting', {
-            templateUrl: './Views/job_listing.html',
+            templateUrl: 'Views/job_listing.html',
             controller: 'HomeController'
         })
         .when('/signup', {
-            templateUrl: './Views/signup.html',
+            templateUrl: 'Views/signup.html',
             controller: 'HomeController'
         })
         .when('/signupemployers', {
-            templateUrl: './Views/signup_employers.html',
+            templateUrl: 'Views/signup_employers.html',
             controller: 'HomeController'
         })
         .when('/account', {
-            templateUrl: './Views/account.html',
+            templateUrl: 'Views/account.html',
             controller: 'HomeController'
         })
         .when('/login', {
-            templateUrl: './Views/login.html',
+            templateUrl: 'Views/login.html',
             controller: 'HomeController'
         })
         .when('/post', {
-            templateUrl: './Views/post.html',
+            templateUrl: 'Views/post.html',
             controller: 'HomeController'
         })
         .when('/postmanagement', {
-            templateUrl: './Views/post_management.html',
+            templateUrl: 'Views/post_management.html',
             controller: 'HomeController'
         })
         .when('/profile', {
-            templateUrl: './Views/profile.html',
+            templateUrl: 'Views/profile.html',
             controller: 'ProfileController'
         })
         .when('/seeker', {
-            templateUrl: './views/seeker/seeker.html',
+            templateUrl: 'views/seeker/seeker.html',
         })
         .when('/job_details', {
-            templateUrl: './views/seeker/job_details.html',
+            templateUrl: 'views/seeker/job_details.html',
         })
         .when('/history', {
-            templateUrl: './views/seeker/history.html',
+            templateUrl: 'views/seeker/history.html',
         })
         .when('/post_management_admin', {
-            templateUrl: './Views/post_management_admin.html',
+            templateUrl: 'Views/post_management_admin.html',
         })
         .otherwise({
             redirectTo: '/'
