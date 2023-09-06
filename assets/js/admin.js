f22da0c296a24b6979757716310cd80cb675facb
TuanKha0904 <kha090403@gmail.com>
2023-09-07 02:55:42 +0700

    login_admin

---
+++ assets/js/admin.js
@@ -0,0 +1,17 @@
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById("password-field");
+    var eyeIcon = document.querySelector(".click-eye");
+
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      eyeIcon.classList.remove("fa-eye-slash");
+      eyeIcon.classList.add("fa-eye");
+    } else {
+      passwordField.type = "password";
+      eyeIcon.classList.remove("fa-eye");
+      eyeIcon.classList.add("fa-eye-slash");
+    }
+  }
+
+  // Lắng nghe sự kiện click trên biểu tượng mắt và gọi hàm togglePasswordVisibility()
+  document.querySelector(".click-eye").addEventListener("click", togglePasswordVisibility);
--- assets/js/directive_admin.js
+++ assets/js/directive_admin.js
@@ -21,3 +21,4 @@ app.directive("databg", () => {
     );
   });
 });
+
